59254e9e1a1f6f8262b149cc5a2ad63744ea4643
kunal1406 <53470244+kunal1406@users.noreply.github.com>
2024-08-16 18:00:36 -0500

    Time: 1061 ms (37.75%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,9 +1,21 @@
 # Write your MySQL query statement below
 
-SELECT Department, name as Employee, salary as Salary FROM
+# cross join the Employee and Department table
 
-(SELECT E.id, E.name, E.salary, D.name As Department, DENSE_RANK() over (partition by E.departmentId ORDER BY E.salary DESC) As salary_rank
-FROM Employee E
-Join Department D on E.departmentId = D.id)  as RankedSalaries
+-- Select d.name as Department, e.name as Employee, e.salary from Employee e
+-- cross join Department d
+-- on e.departmentID = d.id
 
-WHERE salary_rank <= 3
+# apply dense rank and give ranking to the employees
+With combined as 
+(Select d.name as Department, e.name as Employee, e.salary from Employee e
+cross join Department d
+on e.departmentID = d.id),
+
+ranks as
+(select *, dense_rank() over(partition by Department order by salary Desc) as rankings from combined)
+
+Select Department, Employee, salary as Salary from ranks 
+where rankings <= 3
+
+# top 3 out of each department
