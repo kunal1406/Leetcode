214026ea90169a529cf38eda3c4e9077f3355aaa
kunal1406 <53470244+kunal1406@users.noreply.github.com>
2024-08-16 21:07:13 -0500

    Time: 296 ms (54.82%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,51 +1,37 @@
 # Write your MySQL query statement below
 
-# SELECT requester_id, accepter_id, accept_date, COUNT(*) OVER(partition by requester_id) as cnt_req FROM RequestAccepted
 
-# {"headers": ["requester_id", "accepter_id", "accept_date", "cnt_req"]
-# , "values": [[1, 2, "2016-06-03", 2],
-#              [1, 3, "2016-06-08", 2], 
-#              [2, 3, "2016-06-08", 1], 
-#              [3, 4, "2016-06-09", 1]]}
-             
-SELECT  requester_id as id, SUM(cnt_req) as num
+# find the person with most friends 
+        # requester id count + accepter_id count
 
-FROM            
-    (SELECT COUNT(*) as cnt_req, requester_id
-    FROM RequestAccepted 
-    group by requester_id
+WITH FriendCount AS (
+    SELECT 
+        requester_id AS id, 
+        COUNT(DISTINCT accepter_id) AS num_friends
+    FROM 
+        RequestAccepted
+    GROUP BY 
+        requester_id
     UNION ALL
-    SELECT COUNT(*) as cnt_acc, accepter_id  
-    FROM RequestAccepted
-    group by accepter_id)
-    as subquery
-    
-Group by requester_id 
-order by num desc
-limit 1
+    SELECT 
+        accepter_id AS id, 
+        COUNT(DISTINCT requester_id) AS num_friends
+    FROM 
+        RequestAccepted
+    GROUP BY 
+        accepter_id
+),
 
+TotalFriends AS (
+    SELECT 
+        id, 
+        SUM(num_friends) AS total_friends
+    FROM 
+        FriendCount
+    GROUP BY 
+        id
+)
 
-# {"headers": ["cnt_req", "requester_id"], 
-# "values": [[2, 1], 
-#            [1, 2], 
-#            [1, 3], 
-#            [1, 2],
-#            [2, 3], 
-#            [1, 4]]}
+select id, total_friends as num from TotalFriends
+order by total_friends desc limit 1
 
-
-
-# {"headers": ["requester_id", "accepter_id", "accept_date", "COUNT(*)"],
-# "values": [[1, 2, "2016-06-03", 2], 
-#            [2, 3, "2016-06-08", 1], 
-#            [3, 4, "2016-06-09", 1], 
-#            [1, 2, "2016-06-03", 1], 
-#            [1, 3, "2016-06-08", 2]]}
-
-
-
-
-# {"headers": ["requester_id", "accepter_id", "accept_date", "COUNT(*)"],
-# "values": [[1, 2, "2016-06-03", 2], 
-#            [2, 3, "2016-06-08", 1], 
-#            [3, 4, "2016-06-09", 1]]}
