708e6d23f956ebb0c9adc5ec3994cf2d5792f442
kunal1406 <53470244+kunal1406@users.noreply.github.com>
2024-09-09 23:38:27 -0500

    Time: 612 ms (60.13%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -4,8 +4,22 @@
 # names of all managers
 # number of employees report directly to them
 
-With manager_id as (select count(*) as counts, Round(avg(age), 0) as average, reports_to from Employees
-group by reports_to 
-having count(*) >= 1) 
+-- With manager_id as (select count(*) as counts, Round(avg(age), 0) as average, reports_to from Employees
+-- group by reports_to 
+-- having count(*) >= 1) 
 
-select employee_id, name, counts  as reports_count, average as average_age from manager_id m Inner join Employees e on m.reports_to = e.employee_id order by e.employee_id
+-- select employee_id, name, counts  as reports_count, average as average_age from manager_id m Inner join Employees e on m.reports_to = e.employee_id order by e.employee_id
+
+SELECT 
+    e.employee_id,
+    e.name,
+    COUNT(r.employee_id) AS reports_count,
+    ROUND(AVG(r.age)) AS average_age
+FROM 
+    Employees e
+JOIN 
+    Employees r ON e.employee_id = r.reports_to
+GROUP BY 
+    e.employee_id, e.name
+ORDER BY 
+    e.employee_id;
