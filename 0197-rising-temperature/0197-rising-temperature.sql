94147afc13555779769f5f7ecd8a0f4a4c5b9c00
kunal1406 <53470244+kunal1406@users.noreply.github.com>
2024-09-21 01:28:36 -0500

    Time: 317 ms (94.13%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
-# SELECT w1.id
-# FROM Weather w1, Weather w2
-# WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+WITH q1 AS (
+  SELECT 
+      *,
+      LAG(temperature) OVER (ORDER BY recordDate) AS previous_day_temperature,
+      LAG(recordDate) OVER (ORDER BY recordDate) AS previous_Date
+  FROM Weather
+)
 
-SELECT w1.id
-FROM Weather w1
-JOIN Weather w2 ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
-WHERE w1.temperature > w2.temperature;
+-- Select the IDs where the temperature is higher than the previous day
+-- and the previous day exists (difference is exactly 1 day)
+SELECT id
+FROM q1
+WHERE temperature > previous_day_temperature
+AND DATEDIFF(recordDate, previous_Date) = 1;
